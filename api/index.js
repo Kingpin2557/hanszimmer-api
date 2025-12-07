0bb67a419c46a57c2239ca213a7c7f0f6165d83e
Kingpin2557 <lode.snaet@student.howest.be>
2025-12-07 19:51:14 +0100

    fix: trying netlify hosting

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
 import {app} from '../init.js';
 
-export default serverless(app);
+// export default serverless(app);
+
+export const handler = serverless(app);
